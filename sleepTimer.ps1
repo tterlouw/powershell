1e22fcf989de3261df001d21ff7b48f3b5f3c143
Theo Terlouw <tterlouw@gmail.com>
2019-07-05 14:23:25 +0200

    some changes to the sleptimer script

--- sleepTimer.ps1
+++ sleepTimer.ps1
@@ -1,7 +1,7 @@
 # Start a timer and output its status every 10 seconds
 
 $timeCounter = 10
-$sleepTime = 120
+$sleepTime = 90
 
 Write-Host "Total sleep time will be $sleeptime seconds"
 
@@ -9,4 +9,5 @@ do {
     Start-Sleep -Seconds 10
     Write-Host "It has been $timeCounter seconds since the sleep timer started."
     $timeCounter = $timeCounter + 10
-} while ($timeCounter -ne 120)
+} while ($timeCounter -ne $sleepTime)
+Write-Host "The sleeptimer finished after $sleepTime seconds"
