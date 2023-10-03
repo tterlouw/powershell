73db9fb39750b0f52f27d524fb5ee117b6c1cb34
theo.terlouw@macaw.nl <theo.terlouw@macaw.nl>
2023-10-03 13:41:54 +0200

    new storage cleanup script

---
+++ cleanStorageAccount.ps1
@@ -0,0 +1,32 @@
+# Connect to azure
+Connect-AzAccount
+
+# Set the variables
+$subscription = ""
+$storageAccountName = ""
+$resourceGroupName = ""
+$containerName = ""
+
+Set-AzContext -Subscription $subscription
+
+$context = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
+
+# Get the blobs in the container
+$blobs = Get-AzStorageBlob -Container $containerName -Context $context
+
+# Get the current date
+$currentDate = Get-Date
+
+# Loop through each blob
+foreach ($blob in $blobs) {
+    # Get the blob's last modified date
+    $lastModifiedDate = $blob.LastModified.UtcDateTime
+
+    # Calculate the difference in days between the current date and the last modified date
+    $daysOld = ($currentDate - $lastModifiedDate).Days
+
+    # If the blob is older than 90 days, delete it
+    if ($daysOld -gt 30) {
+        Remove-AzStorageBlob -Container $containerName -Blob $blob.Name -Context $context
+    }
+}
